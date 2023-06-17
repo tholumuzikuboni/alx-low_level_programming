e75e9953c51ec407c4c8b69db3c60c55fac9cc41
Tholumuzi <tholumuziwelcomekuboni@gmail.com>
2023-06-17 15:47:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}

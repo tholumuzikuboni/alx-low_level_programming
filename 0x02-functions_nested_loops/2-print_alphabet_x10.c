b01dab4150d4d5cc9be4ae40b87d105271e00e2b
Tholumuzi <tholumuziwelcomekuboni@gmail.com>
2023-06-22 12:14:30 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print 10 x alphabet - makev the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c;
+
+	int i = 0;
+
+	{
+
+	while (i <= 9)
+
+	{	
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	_putchar(c);
+
+	}
+
+	_putchar('\n');
+	i++;
+
+	}
+	
+
+}

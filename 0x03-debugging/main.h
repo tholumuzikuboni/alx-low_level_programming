1b20bb59e70cdcfe8c20fe3ec32348d977055028
Tholumuzi <tholumuziwelcomekuboni@gmail.com>
2023-06-29 13:55:50 -0700

    0 > 972

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,4 +8,6 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
